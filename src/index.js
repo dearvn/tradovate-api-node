979fb26bfe414ede2ed6521b6c5567ecda50ee71
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2022-10-03 09:23:57 +0700

    update test

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+function greet(name) {
+    console.log('dsdsds');
+    return `Hello ${name}`;
+}
+exports.greet = greet;
+
+greet()
+
