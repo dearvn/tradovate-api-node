5960571223f5bd2272acd02f59bf31cc76d442e1
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2022-10-16 23:44:13 +0700

    update code

--- src/websocket.js
+++ src/websocket.js
@@ -1,32 +1,12 @@
-import zip from 'lodash.zipobject'
-import JSONbig from 'json-bigint'
-
-import httpMethods from 'http-client'
+import './common'
 import _openWebSocket from 'open-websocket'
 
-const _sessionStorage = {}
-
-const sessionStorage = {
-  setItem: (key, value) => _sessionStorage[key] = value,
-  getItem: (key) => _sessionStorage[key],
-  removeItem: (key) => delete _sessionStorage[key],
-}
-
-const DEMO_URL = 'https://demo.tradovateapi.com/v1'
-const LIVE_URL = 'https://live.tradovateapi.com/v1'
-const WS_DEMO_URL = 'wss://demo.tradovateapi.com/v1/websocket'
-
-const STORAGE_KEY       = 'tradovate-api-access-token'
-const EXPIRATION_KEY    = 'tradovate-api-access-expiration'
-const DEVICE_ID_KEY     = 'tradovate-device-id'
-const AVAIL_ACCTS_KEY   = 'tradovate-api-available-accounts'
-const USER_DATA_KEY     = 'tradovate-user-data'
-
 const endpoints = {
-  base_url: 'https://demo.tradovateapi.com/v1',
-  md: 'wss://md.tradovateapi.com/v1/websocket',
-  demo: 'wss://demo.tradovateapi.com/v1/websocket',
-  live: 'wss://live.tradovateapi.com/v1/websocket',
+    httpDemo: DEMO_URL,
+    httpLive: LIVE_URL,
+    wsMd: WS_MD_URL,
+    wsDemo: WS_DEMO_URL,
+    wsLive: WS_LIVE_URL,
 }
 
 const noop = () => {}
@@ -36,229 +16,6 @@ const wsOptions = {}
 function openWebSocket(url) {
   return _openWebSocket(url, wsOptions)
 }
-
-const setDeviceId = (id) => {
-    sessionStorage.setItem(DEVICE_ID_KEY, id)
-}
-
-const getDeviceId = () => {
-    return sessionStorage.getItem(DEVICE_ID_KEY)
-}
-
-const setAvailableAccounts = accounts => {
-    sessionStorage.setItem(AVAIL_ACCTS_KEY, JSON.stringify(accounts))
-}
-
-/**
- * Returns and array of available accounts or undefined.
- * @returns Account[]
- */
-const getAvailableAccounts = () => {
-    return JSON.parse(sessionStorage.getItem(AVAIL_ACCTS_KEY))
-}
-
-/**
- * Use a predicate function to find an account. May be undefined.
- */
-const queryAvailableAccounts = predicate => {
-    return JSON.parse(getAvailableAccounts()).find(predicate)
-}
-
-const setAccessToken = (token, expiration) => {
-    //if(!token || !expiration) throw new Error('attempted to set undefined token')
-    sessionStorage.setItem(STORAGE_KEY, token)
-    sessionStorage.setItem(EXPIRATION_KEY, expiration)
-}
-
-const getAccessToken = () => {
-    const token = sessionStorage.getItem(STORAGE_KEY)
-    const expiration = sessionStorage.getItem(EXPIRATION_KEY)
-    if(!token) {
-        console.warn('No access token retrieved. Please request an access token.')
-    }
-    return { token, expiration }
-}
-
-const tokenIsValid = expiration => new Date(expiration) - new Date() > 0 
-
-const setUserData = (data) => sessionStorage.setItem(USER_DATA_KEY, JSON.stringify(data))
-const getUserData = () => JSON.parse(sessionStorage.getItem(USER_DATA_KEY))
-
-const waitForMs = t => {
-    return new Promise((res) => {
-        setTimeout(() => {
-            res()
-        }, t)
-    })
-}
-
-const handleRetry = async (data, json) => {
-    const ticket    = json['p-ticket'],
-          time      = json['p-time'],
-          captcha   = json['p-captcha']
-  
-    if(captcha) {
-        console.error('Captcha present, cannot retry auth request via third party application. Please try again in an hour.')
-        return
-    }
-  
-    console.log(`Time Penalty present. Retrying operation in ${time}s`)
-  
-    await waitForMs(time * 1000) 
-    return await connect({ ...data, 'p-ticket': ticket })   
-}
-
-const connect = async ({
-    name,
-    password,
-    appId,
-    appVersion,
-    cid,
-    sec,
-    endpoints,
-    env = 'demo',
-    proxy = ''
-    }) => {
-    let { token, expiration } = getAccessToken()
-
-    if(token && tokenIsValid(expiration)) {
-        console.log('Already connected. Using valid token.') 
-        //const accounts = await tvGet('/account/list')
-        //setAvailableAccounts(accounts)      
-        return token
-    }
-
-    const credentials = {
-        name:       name,
-        password:   password,
-        appId:      appId,
-        appVersion: appVersion,
-        cid:        cid,
-        sec:        sec
-    }
-
-    const privPost = await tvPost({ proxy, endpoints, env })
-
-    const authResponse = await privPost('/auth/accesstokenrequest', credentials, false)
-
-    if(authResponse['p-ticket']) {
-        return await handleRetry({...credentials, proxy: proxy, endpoints:  endpoints, env: env}, authResponse) 
-    } else {
-        const { errorText, accessToken, userId, userStatus, name, expirationTime } = authResponse
-
-        if(errorText) {
-            console.error(errorText)
-            return
-        }
-        
-        setAccessToken(accessToken, expirationTime)
-
-        //const privGet = tvGet({ proxy, endpoints, env })
-
-        //const accounts = await privGet('/account/list')
-
-        //console.log(accounts)
-
-        //setAvailableAccounts(accounts)
-
-        console.log(`Successfully stored access token ${accessToken} for user {name: ${name}, ID: ${userId}, status: ${userStatus}}.`)
-
-        return accessToken
-    }
-}
-
-const tvGet = async ({
-    endpoints,
-    token = '',
-    env = 'demo',
-    proxy = ''
-  }) => async (path, query = {}) => {
-  
-    if (!token) {
-      throw new Error('You need to pass an token to make authenticated calls.')
-    }
-  
-    try {
-      let q = ''
-      if(query) {
-          q = Object.keys(query).reduce((acc, next, i, arr) => {
-              acc += next + '=' + query[next]
-              if(i !== arr.length - 1) acc += '&'
-              return acc
-          }, '?')
-      }
-  
-      console.log('With query:', q.toString() || '<no query>')
-  
-      let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''        
-      if(!baseURL) throw new Error(`[Services:tvGet] => 'env' variable should be either 'live' or 'demo'.`)
-  
-      let url = query !== null
-          ? baseURL + path + q
-          : baseURL + path
-  
-      console.log(url)
-  
-      const res = await fetch(url, {
-          method: 'GET',
-          headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: 'application/json',
-              'Content-Type': 'application/json'
-          },
-          ...(proxy ? { agent: new HttpsProxyAgent(proxy) } : {}),
-      })
-  
-      const js = await res.json()
-  
-      return js
-  
-    } catch(err) {
-        console.error(err)
-    }
-  }
-  
-  const tvPost = async ({
-    endpoints,
-    token = '',
-    env = 'demo',
-    proxy = ''
-  }) => async (path, data = {}, _usetoken = true) => {
-  
-    if (!token && _usetoken == true) {
-      throw new Error('You need to pass an token to make authenticated calls.')
-    }
-  
-    try {
-      
-      const bearer = _usetoken ? { Authorization: `Bearer ${token}` } : {} 
-  
-      let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''        
-      if(!baseURL) throw new Error(`[Services:tvGet] => 'env' variable should be either 'live' or 'demo'.`)
-  
-      let url = baseURL + path
-  
-      console.log(url)
-  
-      const res = await fetch(url, {
-          method: 'POST',
-          headers: {
-              ...bearer,
-              Accept: 'application/json',
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data),
-          ...(proxy ? { agent: new HttpsProxyAgent(proxy) } : {}),
-      })
-  
-      const js = await res.json()
-  
-      return js
-  
-    } catch(err) {
-        console.error(err)
-    }
-  }
 
 /**
  * A generic implementation for the Tradovate real-time APIs WebSocket client. 
@@ -430,7 +187,7 @@ TradovateSocket.prototype.subscribe = async function({url, body, subscription}) 
             case 'md/getchart': {
                 cancelUrl = 'md/cancelChart'
                 cancelBody = { subscriptionId: realtimeId }
-                if(this.listeningURL !== endpoints.md) rej('Cannot subscribe to Chart Data without using the Market Data URL.')
+                if(this.listeningURL !== endpoints.wsMd) rej('Cannot subscribe to Chart Data without using the Market Data URL.')
                 removeListener = self.addListener(data => {
                     if(data.d.charts) {
                         data.d.charts.forEach(chart => chart.id === realtimeId ? subscription(chart) : noop())
@@ -441,7 +198,7 @@ TradovateSocket.prototype.subscribe = async function({url, body, subscription}) 
             case 'md/subscribedom': {
                 cancelUrl = 'md/unsubscribedom'
                 cancelBody = { symbol: body.symbol }
-                if(this.listeningURL !== endpoints.md) rej('Cannot subscribe to DOM Data without using the Market Data URL.')
+                if(this.listeningURL !== endpoints.wsMd) rej('Cannot subscribe to DOM Data without using the Market Data URL.')
                 removeListener = self.addListener(data => {
                     if(data.d.doms) {
                         data.d.doms.forEach(dom => dom.contractId === contractId ? subscription(dom) : noop())
@@ -452,7 +209,7 @@ TradovateSocket.prototype.subscribe = async function({url, body, subscription}) 
             case 'md/subscribequote': {
                 cancelUrl = 'md/unsubscribequote'
                 cancelBody = { symbol: body.symbol }
-                if(this.listeningURL !== endpoints.md) rej('Cannot subscribe to Quote Data without using the Market Data URL.')
+                if(this.listeningURL !== endpoints.wsMd) rej('Cannot subscribe to Quote Data without using the Market Data URL.')
                 removeListener = self.addListener(data => {
                     if(data.d.quotes) {
                         data.d.quotes.forEach(quote => quote.contractId === contractId ? subscription(quote) : noop())
@@ -463,7 +220,7 @@ TradovateSocket.prototype.subscribe = async function({url, body, subscription}) 
             case 'md/subscribehistogram': {
                 cancelUrl = 'md/unsubscribehistogram'
                 cancelBody = { symbol: body.symbol }
-                if(this.listeningURL !== endpoints.md) rej('Cannot subscribe to Histogram Data without using the Market Data URL.')
+                if(this.listeningURL !== endpoints.wsMd) rej('Cannot subscribe to Histogram Data without using the Market Data URL.')
                 removeListener = self.addListener(data => {
                     if(data.d.histograms) {
                         data.d.histograms.forEach(histogram => histogram.contractId === contractId ? subscription(histogram) : noop())
@@ -514,14 +271,7 @@ function prepareMessage(raw) {
 ////////////////////////////////////
 
 export default async (opts) => {
-  if (opts && opts.wsBase) {
-    endpoints.base = opts.wsBase
-  }
-
-  if (opts && opts.wsFutures) {
-    endpoints.futures = opts.wsFutures
-  }
-
+  
   if (opts && opts.proxy) {
     wsOptions.proxy = opts.proxy
   }
@@ -529,16 +279,20 @@ export default async (opts) => {
   const syncSocket = new TradovateSocket({debugLabel: 'sync data'})
 
   const endpoints = {
-    demo: (opts && opts.httpDemo) || DEMO_URL,
-    live: (opts && opts.httpLive) || LIVE_URL,
+    httpDemo: (opts && opts.httpDemo) || DEMO_URL,
+    httpLive: (opts && opts.httpLive) || LIVE_URL,
+    wsMd: (opts && opts.wsMd) || WS_MD_URL,
+    wsDemo: (opts && opts.wsDemo) || WS_DEMO_URL,
+    wsLive: (opts && opts.wsLive) || WS_LIVE_URL
   }
-
 
   const token = await connect({ ...opts, endpoints })
   
-  const userId = ''
+  const userId = getUserData()['ID']
 
-  await syncSocket.connect(WS_DEMO_URL, token)
+  let wsURL = opts.env === 'demo' ? endpoints.wsDemo : opts.env === 'live' ? endpoints.wsLive : ''        
+    
+  await syncSocket.connect(endpoints.wsMd, token)
 
   const subscribe = await syncSocket.subscribe({
       url: 'user/syncrequest',
