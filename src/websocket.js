425d14b6c3879eead6d8bc1246a97eb757b03cdc
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2022-10-16 10:31:04 +0700

    update code

--- src/websocket.js
+++ src/websocket.js
@@ -80,88 +80,173 @@ const waitForMs = t => {
     })
 }
 
-const tvGet = async (endpoint, query = null, env = 'demo') => {
-    const { token } = getAccessToken()
-    try {
-        let q = ''
-        if(query) {
-            q = Object.keys(query).reduce((acc, next, i, arr) => {
-                acc += next + '=' + query[next]
-                if(i !== arr.length - 1) acc += '&'
-                return acc
-            }, '?')
-        }
-
-        console.log('With query:', q.toString() || '<no query>')
-
-        let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''        
-        if(!baseURL) throw new Error(`[Services:tvGet] => 'env' variable should be either 'live' or 'demo'.`)
-
-        let url = query !== null
-            ? baseURL + endpoint + q
-            : baseURL + endpoint
-
-        console.log(url)
-
-        const res = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const js = await res.json()
-
-        return js
-
-    } catch(err) {
-        console.error(err)
-    }
-}
-
-const tvPost = async (endpoint, data, _usetoken = true, env = 'demo') => {
-    const { token } = getAccessToken()
-    const bearer = _usetoken ? { Authorization: `Bearer ${token}` } : {} 
-
-    let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''
-    if(!baseURL) throw new Error(`[Services:tvPost] => 'env' variable should be either 'live' or 'demo'.`)
-
-    try {
-        const res = await fetch(baseURL + endpoint, {
-            method: 'POST',
-            headers: {
-                ...bearer,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-
-        const js = await res.json()
-
-        return js
-
-    } catch(err) {
-        console.error(err)
-    }
-}
-
-const connect = async (data) => {
-    const { token, expiration } = getAccessToken()
-    if(token && tokenIsValid(expiration)) {
-        console.log('Already have an access token. Using existing token.')
+const handleRetry = async (data, json) => {
+    const ticket    = json['p-ticket'],
+          time      = json['p-time'],
+          captcha   = json['p-captcha']
+  
+    if(captcha) {
+        console.error('Captcha present, cannot retry auth request via third party application. Please try again in an hour.')
         return
     }
-    const authResponse = await tvPost('/auth/accesstokenrequest', data, false)
-
-    const { accessToken, expirationTime } = authResponse
-
-    console.log(authResponse)
-    
-    setAccessToken(accessToken, expirationTime)
+  
+    console.log(`Time Penalty present. Retrying operation in ${time}s`)
+  
+    await waitForMs(time * 1000) 
+    return await connect({ ...data, 'p-ticket': ticket })   
 }
+
+const connect = async ({
+    name,
+    password,
+    appId,
+    appVersion,
+    cid,
+    sec,
+    endpoints,
+    env = 'demo',
+    proxy = ''
+    }) => {
+    let { token, expiration } = getAccessToken()
+
+    if(token && tokenIsValid(expiration)) {
+        console.log('Already connected. Using valid token.') 
+        //const accounts = await tvGet('/account/list')
+        //setAvailableAccounts(accounts)      
+        return token
+    }
+
+    const credentials = {
+        name:       name,
+        password:   password,
+        appId:      appId,
+        appVersion: appVersion,
+        cid:        cid,
+        sec:        sec
+    }
+
+    const privPost = tvPost({ proxy, endpoints, env })
+
+    const authResponse = await privPost('/auth/accesstokenrequest', credentials, false)
+
+    if(authResponse['p-ticket']) {
+        return await handleRetry({...credentials, proxy: proxy, endpoints:  endpoints, env: env}, authResponse) 
+    } else {
+        const { errorText, accessToken, userId, userStatus, name, expirationTime } = authResponse
+
+        if(errorText) {
+            console.error(errorText)
+            return
+        }
+        
+        setAccessToken(accessToken, expirationTime)
+
+        //const privGet = tvGet({ proxy, endpoints, env })
+
+        //const accounts = await privGet('/account/list')
+
+        //console.log(accounts)
+
+        //setAvailableAccounts(accounts)
+
+        console.log(`Successfully stored access token ${accessToken} for user {name: ${name}, ID: ${userId}, status: ${userStatus}}.`)
+
+        return accessToken
+    }
+}
+
+const tvGet = async ({
+    endpoints,
+    token = '',
+    env = 'demo',
+    proxy = ''
+  }) => async (path, query = {}) => {
+  
+    if (!token) {
+      throw new Error('You need to pass an token to make authenticated calls.')
+    }
+  
+    try {
+      let q = ''
+      if(query) {
+          q = Object.keys(query).reduce((acc, next, i, arr) => {
+              acc += next + '=' + query[next]
+              if(i !== arr.length - 1) acc += '&'
+              return acc
+          }, '?')
+      }
+  
+      console.log('With query:', q.toString() || '<no query>')
+  
+      let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''        
+      if(!baseURL) throw new Error(`[Services:tvGet] => 'env' variable should be either 'live' or 'demo'.`)
+  
+      let url = query !== null
+          ? baseURL + path + q
+          : baseURL + path
+  
+      console.log(url)
+  
+      const res = await fetch(url, {
+          method: 'GET',
+          headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+          },
+          ...(proxy ? { agent: new HttpsProxyAgent(proxy) } : {}),
+      })
+  
+      const js = await res.json()
+  
+      return js
+  
+    } catch(err) {
+        console.error(err)
+    }
+  }
+  
+  const tvPost = async ({
+    endpoints,
+    token = '',
+    env = 'demo',
+    proxy = ''
+  }) => async (path, data = {}, _usetoken = true) => {
+  
+    if (!token && _usetoken == true) {
+      throw new Error('You need to pass an token to make authenticated calls.')
+    }
+  
+    try {
+      
+      const bearer = _usetoken ? { Authorization: `Bearer ${token}` } : {} 
+  
+      let baseURL = env === 'demo' ? endpoints.demo : env === 'live' ? endpoints.live : ''        
+      if(!baseURL) throw new Error(`[Services:tvGet] => 'env' variable should be either 'live' or 'demo'.`)
+  
+      let url = baseURL + path
+  
+      console.log(url)
+  
+      const res = await fetch(url, {
+          method: 'POST',
+          headers: {
+              ...bearer,
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data),
+          ...(proxy ? { agent: new HttpsProxyAgent(proxy) } : {}),
+      })
+  
+      const js = await res.json()
+  
+      return js
+  
+    } catch(err) {
+        console.error(err)
+    }
+  }
 
 /**
  * A generic implementation for the Tradovate real-time APIs WebSocket client. 
@@ -431,9 +516,11 @@ export default async (opts) => {
 
   const syncSocket = new TradovateSocket({debugLabel: 'sync data'})
 
-  const { accessToken, userId } = await getAccessToken(URL, credentials)
+  const token = await connect({ ...opts, endpoints })
+  
+  const userId = ''
 
-  await syncSocket.connect(WS_DEMO_URL, accessToken)
+  await syncSocket.connect(WS_DEMO_URL, token)
 
   const subscribe = await syncSocket.subscribe({
       url: 'user/syncrequest',
